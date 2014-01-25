10895fb89f82cbe54fbea90e1d737caa4773c5d6
Petko Bordjukov <bordjukov@gmail.com>
2014-01-25 15:47:22 +0200

    Add generic home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 InitLabAuth::Application.routes.draw do
+  get "home/index"
+  root "home#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
