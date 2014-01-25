983260954546699322328da581432ad8236a08ce
Petko Bordjukov <bordjukov@gmail.com>
2014-01-25 14:47:59 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InitLabAuth::Application.config.secret_key_base = 'dc6c5afab279cb8b8b97527f6fd68ecbf1c1e211fc45c640b862da87a69e71a133966dfac91b00dde7e4b613f33b1513f82547363724a05fe9c63845a20a5f56'
