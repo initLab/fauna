062c1fe62200ce80e74edbe0cb4206d3a91a8ca5
Petko Bordjukov <bordjukov@gmail.com>
2014-02-04 20:20:01 +0200

    Add CORS access control headers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  before_filter :set_access_control_headers, only: :present
+
   def present
     @users = Arp.present_users
   end
+
+  private
+    def set_access_control_headers
+      response.headers['Access-Control-Allow-Origin'] = '*'
+      response.headers['Access-Control-Request-Method'] = '*'
+    end
 end
