736790730b240bc213dfce8d9135086e9140b23b
Petko Bordjukov <bordjukov@gmail.com>
2014-02-04 20:35:58 +0200

    Validate the URL of the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ActiveRecord::Base
   has_many :phones, foreign_key: 'userid', dependent: :destroy
   has_many :computers, foreign_key: 'userid', dependent: :destroy
 
-  validates :twitter, format: {with: /\A[A-Za-z0-9_]{0,15}\z/, message: 'must be a twitter handle'}
+  validates :twitter, format: {with: /\A[A-Za-z0-9_]{1,15}\z/, message: 'must be a twitter handle'}, allow_blank: true
+  validates :url, format: {with: /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix, message: 'must be a valid http(s) URL'}, allow_blank: true
 
   def twitter=(handle)
     write_attribute :twitter, handle.gsub(/\A@/,'')
