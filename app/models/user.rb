d45e871fdaa93164b44520e2e5f4effcd096d94e
Petko Bordjukov <bordjukov@gmail.com>
2014-02-04 20:31:17 +0200

    Add twitter handle validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ActiveRecord::Base
 
   has_many :phones, foreign_key: 'userid', dependent: :destroy
   has_many :computers, foreign_key: 'userid', dependent: :destroy
+
+  validates :twitter, format: {with: /\A[A-Za-z0-9_]{0,15}\z/, message: 'must be a twitter handle'}
+
+  def twitter=(handle)
+    write_attribute :twitter, handle.gsub(/\A@/,'')
+  end
 end
