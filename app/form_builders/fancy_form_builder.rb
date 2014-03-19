06789a32066574390565d3948359791ce17061ec
Petko Bordjukov <bordjukov@gmail.com>
2014-03-19 16:34:57 +0200

    Implement a fancy form builder

---
+++ app/form_builders/fancy_form_builder.rb
@@ -0,0 +1,24 @@
+class FancyFormBuilder < ActionView::Helpers::FormBuilder
+  (field_helpers - [:label, :check_box, :radio_button, :fields_for, :hidden_field, :file_field]).each do |selector|
+    define_method(selector) do |name, *args|
+      options = args.extract_options!
+      options[:placeholder] ||= @object.class.human_attribute_name(name)
+      options[:class] = [options[:class], 'form-control'].join(' ')
+      if @object.errors[name]
+        options[:data] ||= {}
+        options[:data][:error] = @object.errors[name].to_json
+      end
+      args << options
+      super(name, *args)
+    end
+  end
+
+  def check_box(name, *args)
+    options = args.extract_options!
+    options[:label] ||= @object.class.human_attribute_name(name)
+    args << options
+    label :privacy, class: 'checkbox' do
+      "#{super(name, *args)} #{options[:label]}".html_safe
+    end
+  end
+end
