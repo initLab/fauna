8d578c99d43472349d3fadf0ffcc46eb0c605379
Petko Bordjukov <bordjukov@gmail.com>
2014-05-28 17:53:12 +0300

    Require current password only when changing email/pass

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,33 @@
 class RegistrationsController < Devise::RegistrationsController
+  def update
+    @user = User.find(current_user.id)
+
+    successfully_updated = if needs_password?(@user, params)
+      @user.update_with_password(devise_parameter_sanitizer.sanitize(:account_update))
+    else
+      # remove the virtual current_password attribute
+      # update_without_password doesn't know how to ignore it
+      params[:user].delete(:current_password)
+      @user.update_without_password(devise_parameter_sanitizer.sanitize(:account_update))
+    end
+
+    if successfully_updated
+      set_flash_message :notice, :updated
+      # Sign in the user bypassing validation in case his password changed
+      sign_in @user, :bypass => true
+      redirect_to after_update_path_for(@user)
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def needs_password?(user, params)
+    user.email != params[:user][:email] ||
+      params[:user][:password].present?
+  end
+
   protected
 
   def after_sign_up_path_for(resource)
@@ -8,4 +37,13 @@ class RegistrationsController < Devise::RegistrationsController
       super
     end
   end
+
+  def after_update_path_for(resource)
+    if resource.is_a?(User)
+      edit_user_registration_path
+    else
+      super
+    end
+  end
+
 end
