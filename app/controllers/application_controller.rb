30b8297617543ab347a29f49104e8aecfef7d43c
Petko Bordjukov <bordjukov@gmail.com>
2014-05-28 12:46:16 +0300

    Make the name attribute mandatory for Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up).push(*[:name])
     devise_parameter_sanitizer.for(:account_update).push(*[:name, :url, :twitter, :privacy])
   end
 
