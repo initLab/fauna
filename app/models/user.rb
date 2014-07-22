1ca22b636bf256253e1ac7dbd00a225f6d6460c7
3ightbit <github@3ightbit.net>
2014-07-23 02:20:26 +0300

    Fixed autofocus of registration form; Rearanged user edit forms; Changed setting of hash parameters style;

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+         :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:login]
 
   has_many :network_devices, foreign_key: :owner_id, dependent: :destroy
 
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
     conditions = warden_conditions.dup
 
     if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
     else
       where(conditions).first
     end
