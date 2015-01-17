207214ca6451078302b2665a20d92ae038605c2b
Petko Bordjukov <bordjukov@gmail.com>
2015-01-17 23:39:19 +0200

    Remove Polymer as it is not mature enough
    Signed-off-by: Petko Bordjukov <bordjukov@gmail.com>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require polymer/webcomponents
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
