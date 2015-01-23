0fd18cc5b84885dd7f21f96edd23e0dc21ea7636
Petko Bordjukov <bordjukov@gmail.com>
2015-01-23 22:37:43 +0200

    Bring Capistrano config up to speed
    Recreate Capistrano configuration as there have been several changes to
    it.

--- Capfile
+++ Capfile
@@ -1,10 +1,10 @@
-# Load DSL and Setup Up Stages
+# Load DSL and set up stages
 require 'capistrano/setup'
 
-# Includes default deployment tasks
+# Include default deployment tasks
 require 'capistrano/deploy'
 
-# Includes tasks from other gems included in your Gemfile
+# Include tasks from other gems included in your Gemfile
 #
 # For documentation on these, see for example:
 #
@@ -13,14 +13,16 @@ require 'capistrano/deploy'
 #   https://github.com/capistrano/chruby
 #   https://github.com/capistrano/bundler
 #   https://github.com/capistrano/rails
+#   https://github.com/capistrano/passenger
 #
 # require 'capistrano/rvm'
 # require 'capistrano/rbenv'
 # require 'capistrano/chruby'
 require 'capistrano/bundler'
-require 'capistrano/rails'
+require 'capistrano/rails/assets'
 # require 'capistrano/rails/migrations'
 require 'capistrano/puma'
+# require 'capistrano/passenger'
 
-# Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
-Dir.glob('lib/capistrano/tasks/*.cap').each { |r| import r }
+# Load custom tasks from `lib/capistrano/tasks' if you have any defined
+Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,13 @@
+# config valid only for current version of Capistrano
+lock '3.3.5'
+
 set :application, 'fauna'
 set :repo_url, 'https://github.com/initLab/fauna.git'
 
 # Default branch is :master
-ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
-# Default deploy_to directory is /var/www/my_app
+# Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/fauna/fauna'
 
 # Default value for :scm is :git
@@ -20,10 +23,10 @@ set :deploy_to, '/home/fauna/fauna'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -33,16 +36,6 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 
 namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :publishing, :restart
-
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
@@ -51,4 +44,5 @@ namespace :deploy do
       # end
     end
   end
+
 end
