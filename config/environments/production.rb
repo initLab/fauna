eb70d449191007fa5134b0ea309bd097771d08ff
Petko Bordjukov <bordjukov@gmail.com>
2015-02-26 00:23:22 +0200

    Specify that we want to use Sendmail in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,9 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # Use sendmail for delivering email
+  config.action_mailer.delivery_method = :sendmail
+
   # Use localhost when sending emails in development
   config.action_mailer.default_url_options = {host: 'fauna.initlab.org'}
 
