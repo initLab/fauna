a9c82365d226959debc1e8a80333f612c4fb0673
Petko Bordjukov <bordjukov@gmail.com>
2015-03-01 03:35:21 +0200

    White space fixes to models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,15 +15,14 @@ class User < ActiveRecord::Base
   validates :twitter, format: {with: /\A[A-Za-z0-9_]{1,15}\z/}, allow_blank: true
   validates :url, format: {with: /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix}, allow_blank: true
   validates :name, presence: true
-  validates :github, format: {with: /\A[a-z][a-z-]{,38}\z/i }, allow_blank: true
-  validates :jabber, format: {with: /\A[^@]+@[^@]+\z/ }, allow_blank: true
-  validates :gpg_fingerprint, format: {with: /\A[0-9a-f]{4}( ?)([0-9a-f]{4}\1){4}\1{0,2}([0-9a-f]{4}\1){4}[0-9a-f]{4}\z/i }, allow_blank: true
+  validates :github, format: {with: /\A[a-z][a-z-]{,38}\z/i}, allow_blank: true
+  validates :jabber, format: {with: /\A[^@]+@[^@]+\z/}, allow_blank: true
+  validates :gpg_fingerprint, format: {with: /\A[0-9a-f]{4}( ?)([0-9a-f]{4}\1){4}\1{0,2}([0-9a-f]{4}\1){4}[0-9a-f]{4}\z/i}, allow_blank: true
 
   accepts_nested_attributes_for :phone_numbers, update_only: true, allow_destroy: true, reject_if: :all_blank
 
   attr_accessor :login
   after_validation :normalize_gpg_fingerprint
-
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
