f912a2a03b3c6e0eb5c5d169f9f14593d0b2e4bf
Petko Bordjukov <bordjukov@gmail.com>
2015-04-03 19:48:47 +0300

    Tweak default ability so all board members have it

--- app/authorizers/application_authorizer.rb
+++ app/authorizers/application_authorizer.rb
@@ -8,9 +8,6 @@ class ApplicationAuthorizer < Authority::Authorizer
   # @param [Object] user - whatever represents the current user in your app
   # @return [Boolean]
   def self.default(adjective, user)
-    # 'Whitelist' strategy for security: anything not explicitly allowed is
-    # considered forbidden.
-    false
+    user.has_role? :board_member
   end
-
 end
