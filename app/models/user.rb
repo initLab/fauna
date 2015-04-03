1cd8f29ad0ac6c147734f8ec792514438adc0538
Petko Bordjukov <bordjukov@gmail.com>
2015-04-03 21:56:28 +0300

    Include authorization-related methods to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ require 'digest/md5'
 
 class User < ActiveRecord::Base
   rolify
+  include Authority::Abilities
+  include Authority::UserAbilities
 
   # Include default devise modules. Others available are:
   # :timeoutable and :omniauthable
