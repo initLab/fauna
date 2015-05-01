324c7c35beca87ab1c06fb764e62e1d64d6e7237
Petko Bordjukov <bordjukov@gmail.com>
2015-05-01 05:05:17 +0300

    Fix typo

--- config/initializers/door_status_manager.rb
+++ config/initializers/door_status_manager.rb
@@ -3,4 +3,4 @@ Rails.application.config.door_status_manager = ActiveSupport::OrderedOptions.new
 Rails.application.config.door_status_manager.host = 'http://192.168.232.2'
 Rails.application.config.door_status_manager.timeout = 1
 Rails.application.config.door_status_manager.cache_lifetime = 1.minute
-Rails.application.config.door_status_manager.backend = DoorStatusManager::Pesho.new
+Rails.application.config.door_status_manager_backend = DoorStatusManager::Pesho.new
