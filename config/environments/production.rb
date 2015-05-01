0b224923e67a48a650243197d9cab8a514326d56
Petko Bordjukov <bordjukov@gmail.com>
2015-05-01 04:24:21 +0300

    Shuffle around the door status manager initialization
    The initialization still fails with production deployment. Try to work
    around this.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.door_status_manager_backend = DoorStatusManager::Pesho.new
 end
--- config/initializers/door_status_manager.rb
+++ config/initializers/door_status_manager.rb
@@ -1,6 +1,6 @@
 # Pesho instance for door status updates
 Rails.application.config.door_status_manager = ActiveSupport::OrderedOptions.new
-Rails.application.config.door_status_manager.backend = DoorStatusManager::Pesho
 Rails.application.config.door_status_manager.host = 'http://192.168.232.2'
 Rails.application.config.door_status_manager.timeout = 1
 Rails.application.config.door_status_manager.cache_lifetime = 1.minute
+Rails.application.config.door_status_manager.backend = DoorStatusManager::Pesho.new
