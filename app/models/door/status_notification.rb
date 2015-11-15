5741a6aea473df898d40f14325354726324d74e7
Petko Bordjukov <bordjukov@gmail.com>
2015-11-15 19:33:07 +0200

    IRC Spam

--- app/models/door/status_notification.rb
+++ app/models/door/status_notification.rb
@@ -6,5 +6,22 @@ module Door
 
     validates :door, presence: true, inclusion: {in: ['Open', 'Closed']}
     validates :latch, presence: true, inclusion: {in: ['Unlocked', 'Locked']}
+
+    def public_message
+      message = 'The door is now '
+
+      previous_status = StatusNotification.find_by(id: id - 1)
+
+      if previous_status.present?
+        changes = []
+        changes << latch if previous_status.latch != latch
+        changes << door if previous_status.door != door
+        message += changes.join(' and ')
+      else
+        message += "#{latch} and #{door}"
+      end
+
+      message
+    end
   end
 end
