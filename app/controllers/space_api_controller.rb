905ec87f908dc72abc5dfbb7abb330b9cafa465e
Petko Bordjukov <bordjukov@gmail.com>
2016-07-24 01:15:49 +0300

    Add a CORS header for the Space API endpoint

--- app/controllers/space_api_controller.rb
+++ app/controllers/space_api_controller.rb
@@ -1,5 +1,15 @@
 class SpaceApiController < ApplicationController
+  before_action :set_access_control_headers, only: :status
+
   def status
     @space_api = SpaceApi.new
   end
+
+    private
+    def set_access_control_headers
+      if request.format.json?
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Request-Method'] = '*'
+      end
+    end
 end
