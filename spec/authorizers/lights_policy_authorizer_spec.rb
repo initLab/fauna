bb0c5a33b019e4341e264dc1d898a17e3035fad6
Petko Bordjukov <bordjukov@gmail.com>
2016-08-06 15:25:11 +0300

    Add a spec for the Lights Policy Authorizer

---
+++ spec/authorizers/lights_policy_authorizer_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe LightsPolicyAuthorizer do
+  describe 'class' do
+    it 'lets trusted users create' do
+      user = create :trusted_member
+      expect(LightsPolicyAuthorizer).to be_creatable_by user
+    end
+
+    it 'lets board members create' do
+      user = create :board_member
+      expect(LightsPolicyAuthorizer).to be_creatable_by user
+    end
+
+    it 'doesn\'t let other users create' do
+      user = create :user
+      expect(LightsPolicyAuthorizer).to_not be_creatable_by user
+    end
+  end
+end
