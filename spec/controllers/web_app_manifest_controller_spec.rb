fcb0b4ef17182e61bd3f4b99b5b9412f305d7c20
Vencislav Atanasov <user890104@users.noreply.github.com>
2016-08-07 00:08:57 +0300

    Add unit test for manifest, fix turbolinks, add dummy service worker

---
+++ spec/controllers/web_app_manifest_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe WebAppManifestController, type: :controller do
+  describe '#GET manifest' do
+    it 'returns HTTP 200 Success' do
+      get :manifest, format: :json
+      expect(response.code).to eq '200'
+    end
+  end
+end
