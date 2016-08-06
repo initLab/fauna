b807d989dffad57e823b3e912325a591a7935deb
Vencislav Atanasov <user890104@users.noreply.github.com>
2016-08-07 00:50:57 +0300

    Remove useless header from Space API response

--- app/controllers/space_api_controller.rb
+++ app/controllers/space_api_controller.rb
@@ -9,7 +9,6 @@ class SpaceApiController < ApplicationController
     def set_access_control_headers
       if request.format.json?
         response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Request-Method'] = '*'
       end
     end
 end
