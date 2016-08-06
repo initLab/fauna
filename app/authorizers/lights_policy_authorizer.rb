dad223390d194796cb6c1749968af2583b2852bf
Petko Bordjukov <bordjukov@gmail.com>
2016-08-06 18:39:24 +0300

    Make the lights policies readable by trusted members

--- app/authorizers/lights_policy_authorizer.rb
+++ app/authorizers/lights_policy_authorizer.rb
@@ -2,4 +2,8 @@ class LightsPolicyAuthorizer < ApplicationAuthorizer
   def self.creatable_by?(user)
     user.has_role?(:trusted_member) or user.has_role?(:board_member)
   end
+
+  def self.readable_by?(user)
+    creatable_by? user
+  end
 end
--- spec/authorizers/lights_policy_authorizer_spec.rb
+++ spec/authorizers/lights_policy_authorizer_spec.rb
@@ -16,5 +16,20 @@ describe LightsPolicyAuthorizer do
       user = create :user
       expect(LightsPolicyAuthorizer).to_not be_creatable_by user
     end
+
+    it 'lets trusted users read' do
+      user = create :trusted_member
+      expect(LightsPolicyAuthorizer).to be_readable_by user
+    end
+
+    it 'lets board members read' do
+      user = create :board_member
+      expect(LightsPolicyAuthorizer).to be_readable_by user
+    end
+
+    it 'doesn\'t let other users read' do
+      user = create :user
+      expect(LightsPolicyAuthorizer).to_not be_readable_by user
+    end
   end
 end
