a667de61ebc67609ff39117cd5dfc08ea7019337
Petko Bordjukov <bordjukov@gmail.com>
2016-08-11 01:08:48 +0300

    Add more optional scopes

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -57,7 +57,8 @@ Doorkeeper.configure do
   # For more information go to
   # https://github.com/doorkeeper-gem/doorkeeper/wiki/Using-Scopes
   default_scopes  :public
-  # optional_scopes :write, :update
+
+  optional_scopes :email, :door_control, :door_latch_control
 
   # Change the way client credentials are retrieved from the request object.
   # By default it retrieves first from the `HTTP_AUTHORIZATION` header, then
