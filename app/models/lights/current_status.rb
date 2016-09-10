f22d9ca29dc3c4e12c1c3a59f2f04e68cd815d17
Petko Bordjukov <bordjukov@gmail.com>
2016-09-10 20:52:42 +0300

    Introduce a Lights::CurrentStatus model

---
+++ app/models/lights/current_status.rb
@@ -0,0 +1,13 @@
+class Lights::CurrentStatus
+  def initialize
+    @policy_manager = Rails.application.config.lights_policy_manager.new
+  end
+
+  def policy
+    @policy_manager.policy
+  end
+
+  def status
+    @policy_manager.status
+  end
+end
