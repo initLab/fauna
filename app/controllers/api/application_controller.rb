511a1eff0d4594f2f22d40b3d481bc91d1fbc33e
Petko Bordjukov <bordjukov@gmail.com>
2016-10-05 01:56:29 +0300

    Move the set_access_control_headers
    Move the set_access_control_headers method to the
    Api::PublicApiExposingController concern as it seems to fit in better
    there.

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,9 +1,2 @@
 class Api::ApplicationController < ::ApplicationController
-  private
-
-  def set_access_control_headers
-    if request.format.json?
-      response.headers['Access-Control-Allow-Origin'] = '*'
-    end
-  end
 end
--- app/controllers/concerns/api/public_api_exposing_controller.rb
+++ app/controllers/concerns/api/public_api_exposing_controller.rb
@@ -6,4 +6,12 @@ module Api::PublicApiExposingController
   included do
     before_action :set_access_control_headers
   end
+
+  private
+
+  def set_access_control_headers
+    if request.format.json?
+      response.headers['Access-Control-Allow-Origin'] = '*'
+    end
+  end
 end
