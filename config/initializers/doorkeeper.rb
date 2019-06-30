69cb30bb756c3fe51c4d7ce6ffacdcdfcd5cdeb9
Petko Bordjukov <bordjukov@gmail.com>
2019-06-30 14:34:07 +0300

    Define a `resource_owner_authenticator` callback for Doorkeeper

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -6,10 +6,7 @@ Doorkeeper.configure do
 
   # This block will be called to check whether the resource owner is authenticated or not.
   resource_owner_authenticator do
-    raise "Please configure doorkeeper resource_owner_authenticator block located in #{__FILE__}"
-    # Put your resource owner authentication logic here.
-    # Example implementation:
-    #   User.find_by_id(session[:user_id]) || redirect_to(new_user_session_url)
+    authenticate_user!
   end
 
   # If you didn't skip applications controller from Doorkeeper routes in your application routes.rb
