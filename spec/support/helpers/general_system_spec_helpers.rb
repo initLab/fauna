5ee41eabf8c991ad76f34a14cc97aeea876250c9
Petko Bordjukov <bordjukov@gmail.com>
2019-08-14 23:20:37 +0300

    Specify locale when registering a user

--- spec/support/helpers/general_system_spec_helpers.rb
+++ spec/support/helpers/general_system_spec_helpers.rb
@@ -2,16 +2,18 @@
 
 module GeneralSystemSpecHelpers
   def register_a_new_account
-    visit root_path
-    click_on 'Вход'
-    click_on 'Регистрация'
-    fill_in 'Потребителско име', with: 'foo'
-    fill_in 'Име', with: 'foo', match: :first
-    fill_in 'Имейл', with: 'foo@example.com'
-    fill_in 'Парола', with: '123qweASD'
-    fill_in 'Потвърждение на парола', with: '123qweASD'
-    click_on "Регистрирай ме"
-    visit user_confirmation_path(confirmation_token: User.first.confirmation_token)
+    I18n.with_locale(:bg) do
+      visit root_path
+      click_on 'Вход'
+      click_on 'Регистрация'
+      fill_in 'Потребителско име', with: 'foo'
+      fill_in 'Име', with: 'foo', match: :first
+      fill_in 'Имейл', with: 'foo@example.com'
+      fill_in 'Парола', with: '123qweASD'
+      fill_in 'Потвърждение на парола', with: '123qweASD'
+      click_on "Регистрирай ме"
+      visit user_confirmation_path(confirmation_token: User.first.confirmation_token)
+    end
   end
 
   def sign_in_with_the_new_account
