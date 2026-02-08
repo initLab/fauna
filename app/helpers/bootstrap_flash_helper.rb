676b9e7376dd9d9ff59ecba65515fe6d31b6cb8e
Albert Stefanov <albert@kmail.bg>
2026-02-08 10:55:02 +0200

    frontend: use the same bootstrap theme as space.initlab.org

--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module BootstrapFlashHelper
   ALERT_TYPES_MAP = {
     notice: :success,
@@ -5,19 +7,24 @@ module BootstrapFlashHelper
     error: :danger,
     info: :info,
     warning: :warning
-  }
+  }.freeze
 
   def bootstrap_flash
     safe_join(flash.each_with_object([]) do |(type, message), messages|
                 next if message.blank? || !message.respond_to?(:to_str)
+
                 type = ALERT_TYPES_MAP.fetch(type.to_sym, type)
                 messages << flash_container(type, message)
               end, "\n").presence
   end
 
   def flash_container(type, message)
-    content_tag :div, class: "alert alert-#{type}" do
-      safe_concat(message)
+    content_tag :div, class: "card border-#{type}" do
+      content_tag :div, class: 'card-body' do
+        content_tag :div, class: 'card-text' do
+          safe_concat(message)
+        end
+      end
     end
   end
 end
