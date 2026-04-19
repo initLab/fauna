f5c36e98ae735ae1bfe444ac4267949a0d3a969b
Albert Stefanov <aastefanov@outlook.com>
2026-04-19 11:49:19 +0300

    Redirect homepage to space

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,5 @@
 class DashboardController < ApplicationController
+  def index
+    redirect_to 'https://space.initlab.org', allow_other_host: true
+  end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -22,4 +22,13 @@ class RegistrationsController < Devise::RegistrationsController
       respond_with resource
     end
   end
+
+  def after_sign_up_path_for(_resource)
+    user_network_devices_path
+  end
+
+  def after_inactive_sign_up_path_for(_resource)
+    new_user_session_path
+  end
+
 end
